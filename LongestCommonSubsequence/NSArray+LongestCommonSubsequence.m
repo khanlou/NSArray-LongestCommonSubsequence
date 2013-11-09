8169841a58d61b802b88fc3620c1d3cb0b1e7a4d
Soroush Khanlou <soroushkhanlou@gmail.com>
2013-11-08 22:40:42 -0500

    only calculate the added and removed indexes if the user asked for them

--- LongestCommonSubsequence/NSArray+LongestCommonSubsequence.m
+++ LongestCommonSubsequence/NSArray+LongestCommonSubsequence.m
@@ -10,7 +10,7 @@
 
 @implementation NSArray (LongestCommonSubsequence)
 
-- (NSIndexSet*) indexesOfCommonElementsWithArray:(NSArray*)array{
+- (NSIndexSet*) indexesOfCommonElementsWithArray:(NSArray*)array {
     return [self indexesOfCommonElementsWithArray:array addedIndexes:nil removedIndexes:nil];
 }
 
@@ -18,17 +18,17 @@
     
     NSInteger lengths[self.count+1][array.count+1];
     
-	for (NSInteger i = self.count; i >= 0; i--) {
-	    for (NSInteger j = array.count; j >= 0; j--) {
+    for (NSInteger i = self.count; i >= 0; i--) {
+        for (NSInteger j = array.count; j >= 0; j--) {
             
             if (i == self.count || j == array.count) {
                 lengths[i][j] = 0;
             } else if ([self[i] isEqual:array[j]]) {
-                 lengths[i][j] = 1 + lengths[i+1][j+1];
+                lengths[i][j] = 1 + lengths[i+1][j+1];
             } else {
                 lengths[i][j] = MAX(lengths[i+1][j], lengths[i][j+1]);
             }
-	    }
+        }
     }
     
     NSMutableIndexSet *commonIndexes = [NSMutableIndexSet indexSet];
@@ -44,40 +44,40 @@
         }
     }
     
-    NSMutableIndexSet *_removedIndexes = [NSMutableIndexSet indexSet];
-    
-    for (NSInteger i = 0; i < self.count; i++) {
-        if (![commonIndexes containsIndex:i]) {
-            [_removedIndexes addIndex:i];
-        }
-    }
-    
-    
-    NSArray *commonObjects = [self objectsAtIndexes:commonIndexes];
-    
-    NSMutableIndexSet *_addedIndexes = [NSMutableIndexSet indexSet];
-    for (NSInteger i = 0, j = 0; i < commonObjects.count || j < array.count; ) {
-        if (i < commonObjects.count && j < array.count && [commonObjects[i] isEqual:array[j]]) {
-            i++;
-            j++;
-        } else {
-            NSInteger currentIndex = i;
-            while ([_addedIndexes containsIndex:currentIndex]) {
-                currentIndex++;
-            }
-            [_addedIndexes addIndex:currentIndex];
-            j++;
-        }
-    }
-    
     if (removedIndexes) {
+        NSMutableIndexSet *_removedIndexes = [NSMutableIndexSet indexSet];
+        
+        for (NSInteger i = 0; i < self.count; i++) {
+            if (![commonIndexes containsIndex:i]) {
+                [_removedIndexes addIndex:i];
+            }
+        }
         *removedIndexes = _removedIndexes;
     }
+    
+    
     if (addedIndexes) {
+        NSArray *commonObjects = [self objectsAtIndexes:commonIndexes];
+        
+        NSMutableIndexSet *_addedIndexes = [NSMutableIndexSet indexSet];
+        for (NSInteger i = 0, j = 0; i < commonObjects.count || j < array.count; ) {
+            if (i < commonObjects.count && j < array.count && [commonObjects[i] isEqual:array[j]]) {
+                i++;
+                j++;
+            } else {
+                NSInteger currentIndex = i;
+                while ([_addedIndexes containsIndex:currentIndex]) {
+                    currentIndex++;
+                }
+                [_addedIndexes addIndex:currentIndex];
+                j++;
+            }
+        }
+        
         *addedIndexes = _addedIndexes;
     }
     return commonIndexes;
-
+    
 }
 
 @end
